0a67a834b5cc72e1b7b519671fa5c38ce51e7083
quheng <494862190@qq.com>
2017-03-14 21:26:51 +0800

    fix some bugs

--- script/bag_of_words_builder.py
+++ script/bag_of_words_builder.py
@@ -14,11 +14,11 @@ from tqdm import tqdm
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 print(BASE_DIR)
-RAW_DATA_DIR = f'{BASE_DIR}/raw_data'
-BAG_OF_WORDS = f'{BASE_DIR}/data/bag_of_words.json'
+RAW_DATA_DIR = os.path.join(BASE_DIR, 'raw_data')
+BAG_OF_WORDS =  os.path.join(BASE_DIR, 'data/bag_of_words.json')
 
 STOP_WORDS = set(line.strip() for line in open(f'{BASE_DIR}/script/stop_words.txt', 'r', encoding='utf-8').readlines())
-STOP_WORDS.add("生物谷")
+STOP_WORDS.add('生物谷')
 
 
 def build_bag_of_words_model():
@@ -32,7 +32,7 @@ def build_bag_of_words_model():
         print(f'got {len(file_names)} files')
         pb_file_names = tqdm(file_names)
         for filename in pb_file_names:
-            pb_file_names.set_description("Processing %s" % filename)
+            pb_file_names.set_description('Processing %s' % filename)
             full_file_name = os.path.join(parent, filename)
             with open(full_file_name, 'r', encoding='utf-8') as raw_data:
                 try:
@@ -49,10 +49,10 @@ def build_bag_of_words_model():
                     count += 1
                 except Exception as exception:
                     err += 1
-                    print(f"Error during process {filename}, error message: {exception}")
+                    print(f'Error during process {filename}, error message: {exception}')
     with open(BAG_OF_WORDS, 'w', encoding='utf-8') as result_file:
         json.dump(bag_of_word, result_file, ensure_ascii=False)
-    print(f"finish, {count} success, {err} failed")
+    print(f'finish, {count} success, {err} failed')
 
 if __name__ == '__main__':
     build_bag_of_words_model()
