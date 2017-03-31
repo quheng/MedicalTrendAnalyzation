dacd3f0ab6b790e99f9c96c02226d2cf4389f3ff
quheng <494862190@qq.com>
2017-03-31 23:15:08 +0800

    update dependencies

--- visualization/server/app.js
+++ visualization/server/app.js
@@ -1,37 +1,10 @@
-import path from 'path'
 import express from 'express'
-import morgan from 'morgan'
-import webpack from 'webpack'
-import config from './webpackConfig'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import DashboardPlugin from 'webpack-dashboard/plugin'
-const ROOT = __dirname
+import path from 'path'
+import webpackConfigure from './webpackConfig'
 
 const app = express()
+webpackConfigure(app)
 
-const compiler = webpack(config)
-compiler.apply(new DashboardPlugin())
-const options = {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}
-app.use(webpackDevMiddleware(compiler, options))
-app.use(webpackHotMiddleware(compiler, options))
+app.use('/*', (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')))
 
-app.use(morgan('dev'))
-app.use('/static', express.static(path.join(ROOT, 'static')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(ROOT, 'index.html'))
-})
-
-const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 4000
-
-app.listen(port, host, err => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.info(`listening at ${host}:${port}`)
-})
+export default app
--- visualization/server/index.js
+++ visualization/server/index.js
@@ -1,12 +1,12 @@
 import app from './app'
 
 const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 2222
+const port = process.env.PORT || 2333
 
 app.listen(port, host, (err) => {
   if (err) {
-    console.error('failed to start up hatu', err)
+    console.error('failed to start up chalk', err)
     process.exit(-1)
   }
-  console.info(`hatu listening at ${host}:${port}`)
+  console.info(`chalk listening at ${host}:${port}`)
 })
