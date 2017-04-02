64b3a19b2d5dad4610e7035b84167a18f069fdfd
quheng <494862190@qq.com>
2017-04-02 13:46:43 +0800

    update api

--- visualization/server/api.js
+++ visualization/server/api.js
@@ -3,8 +3,12 @@ import _ from 'lodash'
 import fs from 'fs'
 
 const wordsRelationshipPath = '../data/words_relationship.json'
+const ldaTopicPath = '../data/lda/topic.json'
+const ldaDocInfoPath = '../data/lda/doc.json'
 
 const wordsRelationship = JSON.parse(fs.readFileSync(wordsRelationshipPath))
+const ldaDocInfo = JSON.parse(fs.readFileSync(ldaDocInfoPath))
+const ldaTopic = JSON.parse(fs.readFileSync(ldaTopicPath))
 
 const apiRouter = express.Router()
 
@@ -16,6 +20,14 @@ apiRouter.get('/words-relationship', (req, res) => {
 apiRouter.get('/lda', (req, res) => {
   const limit = req.query['limit']
   res.json(_.slice(wordsRelationship, 0, limit))
+})
+
+apiRouter.get('/lda-topic', (req, res) => {
+  res.json(ldaTopic)
+})
+
+apiRouter.get('/lda-doc', (req, res) => {
+  res.json(ldaDocInfo)
 })
 
 export default apiRouter
