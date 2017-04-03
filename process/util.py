bd021572d43adb1f935505c65a55422a4788bd5e
quheng <494862190@qq.com>
2017-04-03 19:12:47 +0800

    add util

---
+++ process/util.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+RAW_DATA_DIR = os.path.join(BASE_DIR, 'raw_data')
+TOPIC_PATH = os.path.join(BASE_DIR, 'data/lda/topic.json')
+DOC_PATH = os.path.join(BASE_DIR, 'data/lda/doc.json')
+LDA_MODEL_PATH = os.path.join(BASE_DIR, 'data/lda/lda_model.pkl')
+
+STOP_WORDS_PATH = os.path.join(BASE_DIR, 'process/stop_words.txt')
+
+STOP_WORDS = set(line.strip() for line in open(STOP_WORDS_PATH, 'r', encoding='utf-8').readlines())
+STOP_WORDS.add('生物谷')
