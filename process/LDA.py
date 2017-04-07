45468a7b6032515f4cbca4ef8ce54708ec471697
quheng <494862190@qq.com>
2017-04-07 11:14:24 +0800

    add perplexity

--- process/LDA.py
+++ process/LDA.py
@@ -5,6 +5,7 @@ import json
 import pickle
 import os
 import logging
+import sys
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
@@ -18,12 +19,7 @@ from util import LDA_MODEL_PATH
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-config = json.load(open(os.path.join(BASE_DIR, 'process', 'LDA_config.json'), 'r'))
-max_df = config['max_df']
-min_df = config['min_df']
-topic_amount = config['topic_amount']
-topic_keywords = config['topic_keywords']
-max_iter = config['max_iter']
+CONFIG_PATH = os.path.join(BASE_DIR, 'process', 'LDA_config.json')
 
 def __get_row_data():
     """
@@ -44,7 +40,7 @@ def __get_row_data():
     return files_info, raw_data
 
 
-def __vectorized(raw_data):
+def __vectorized(raw_data, max_df, min_df):
     logging.info('extracting tf features')
     t0 = time.time()
     vectorizer = CountVectorizer(max_df=max_df,
@@ -57,10 +53,11 @@ def __vectorized(raw_data):
     return vectorizer, tf
 
 
-def __build_lda_model(tf):
+def __build_lda_model(tf, topic_amount, max_iter):
     t0 = time.time()
     logging.info('building lda model')
     lda = LatentDirichletAllocation(n_topics=topic_amount,
+                                    learning_method='online',
                                     max_iter=max_iter,
                                     learning_offset=50.,
                                     random_state=0)
@@ -75,19 +72,40 @@ def __topic_list(lda, feature_names):
         topic_list.append([feature_names[i] for i in topic.argsort()[:-topic_keywords - 1:-1]])
     return topic_list
 
-def __set_lda_info_to_file_info(file_info, lda_model):
+
+def __set_lda_info_to_file_info(file_info, tf, lda):
+    lda_model = lda.transform(tf)
+
     for index, item in enumerate(file_info):
         item['lda'] = lda_model[index].tolist()
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        config = json.loads(sys.argv[1])
+    else:
+        config = json.load(open(CONFIG_PATH, 'r'))
+
+    max_df = config['max_df']
+    min_df = config['min_df']
+    topic_amount = config['topic_amount']
+    topic_keywords = config['topic_keywords']
+    max_iter = config['max_iter']
+    is_saving = 'is_saving' in config and config['is_saving']
+
     file_info, raw_data = __get_row_data()
-    vectorizer, tf = __vectorized(raw_data)
-    lda = __build_lda_model(tf)
+
+    vectorizer, tf = __vectorized(raw_data, max_df, min_df)
+    lda = __build_lda_model(tf, topic_amount, max_iter)
     topic_list = __topic_list(lda, vectorizer.get_feature_names())
-    __set_lda_info_to_file_info(file_info, lda.transform(tf))
-    logging.info('saving model')
-    vectorizer.tokenizer = None # we can not pickle jieba due to lock
-    pickle.dump(lda, open(LDA_MODEL_PATH, 'wb'))
-    pickle.dump(vectorizer, open(VEC_MODEL_PATH, 'wb'))
-    json.dump(topic_list, open(TOPIC_PATH, 'w'), ensure_ascii=False)
-    json.dump(file_info, open(DOC_PATH, 'w'), ensure_ascii=False)
+    __set_lda_info_to_file_info(file_info, tf, lda)
+    config['perplexity'] = lda.perplexity(tf)
+
+    if is_saving:
+        logging.info('saving model')
+        vectorizer.tokenizer = None  # we can not pickle jieba due to lock
+        pickle.dump(lda, open(LDA_MODEL_PATH, 'wb'))
+        pickle.dump(vectorizer, open(VEC_MODEL_PATH, 'wb'))
+        json.dump(topic_list, open(TOPIC_PATH, 'w'), ensure_ascii=False)
+        json.dump(file_info, open(DOC_PATH, 'w'), ensure_ascii=False)
+        json.dump(config, open(CONFIG_PATH, 'w'), ensure_ascii=False)
+    print(json.dumps(config, ensure_ascii=False))
