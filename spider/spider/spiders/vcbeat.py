c7086fa4798a349cde0dcd3154ce84f9696ece2e
quheng <494862190@qq.com>
2017-05-01 16:25:32 +0800

    finish vcbeat spider

--- spider/spider/spiders/vcbeat.py
+++ spider/spider/spiders/vcbeat.py
@@ -1,5 +1,5 @@
 import scrapy
-import request
+import requests
 import json
 
 class VcbeatSpider(scrapy.Spider):
@@ -18,22 +18,17 @@ class VcbeatSpider(scrapy.Spider):
                 break
             data = content['data']
             for item in data:
-                yield scrapy.Request(url=f'{url}/{item[id]}', callback=self.parse)
+                yield scrapy.Request(url=f'{url}/{item[id]}', 
+                    callback=self.parse(item['publish_time'], item['title']))
 
 
 
-    def parse(self, response):
-        # response.css('li.select a::text').extract()
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
-        if response.css('li.select a::text').extract_first() in self.category:
-            date = response.css('div.title5 p::text').extract_first().split(' ')[1]
-            title = response.css('title::text').extract_first()
-            cntx = ''.join(response.css('div.text3 p::text').extract())
+    def parse(self, date, title):
+        def lam(self, response):
+            content = response.xpath('//div[@class="row"]/div[@id="article-detail"]/div[@id="article-content"]/p/span/text()').extract()
+            cntx = ''.join(content)
             filename = f'{date}:{title}.txt'
             with open(f'data/{filename}', 'w') as f:
                 f.write(cntx)
             self.log('Saved file %s' % filename)
+        return lam
