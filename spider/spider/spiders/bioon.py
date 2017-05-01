b0e543e776c09ed022377e7dd14d42813e9bbdc5
quheng <494862190@qq.com>
2017-05-01 21:15:12 +0800

    update setting

--- spider/spider/spiders/bioon.py
+++ spider/spider/spiders/bioon.py
@@ -4,8 +4,11 @@ import scrapy
 
 class BioonSpider(scrapy.Spider):
     name = "bioon"
-    start_urls = ['http://news.bioon.com/research/list-%d.html'%x for x in range(1,50)]
-    
+    start_urls = ['http://news.bioon.com/mobileMedical/research-%d.html'%x for x in range(1,51)]
+    start_urls.extend(['http://news.bioon.com/mobileMedical/company-%d.html'%x for x in range(1,39)])
+    start_urls.extend(['http://news.bioon.com/mobileMedical/products-%d.html'%x for x in range(1,34)])
+    start_urls.extend(['http://news.bioon.com/mobileMedical/industry-%d.html'%x for x in range(1,51)])
+
     def parse(self, response):
         for div in response.css('div.cntx'):
             href = div.css('a::attr(href)').extract_first()
@@ -20,11 +23,7 @@ class BioonSpider(scrapy.Spider):
             cntx = ''.join(response.css('div.text3 p::text').extract())
             sat = date + ":" + title.replace("/",",").replace(":","-")
             filename = '%s.txt' % sat
-            with open(filename, 'w') as f:
-                f.write(date)
-                f.write("\n")
-                f.write(title)
-                f.write("\n")
+            with open(f'bioon/{filename}', 'w') as f:
                 f.write(cntx)
             self.log('Saved file %s' % filename)
         return href
