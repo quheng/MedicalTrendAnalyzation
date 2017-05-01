fde2d4f9fc411c031b679e946375f7f77beee46d
quheng <494862190@qq.com>
2017-05-01 16:29:01 +0800

    fix a bug

--- spider/spider/spiders/vcbeat.py
+++ spider/spider/spiders/vcbeat.py
@@ -3,22 +3,24 @@ import requests
 import json
 
 class VcbeatSpider(scrapy.Spider):
-    name = "bioon"
+    name = "vcbeat"
 
     def start_requests(self):
         article_list_url = 'http://www.vcbeat.net/Index/Index/ajaxGetArticleList'
         article_url = 'http://www.vcbeat.net/Index/Index/ajaxGetArticleList'
-        categoryId = 'http://www.vcbeat.net'
+        categoryId = '2999'
         while True:
             page = 1
-            res = requests.post(url, 
+            res = requests.post(article_list_url, 
                 data = {'categoryId':categoryId, 'page':page})
-            content = json.loads(res.content.decode('utf-8-sig'))
+            res_content = res.content.decode('utf-8-sig')
+            print(res_content)
+            content = json.loads(res_content)
             if 'data' not in content:
                 break
             data = content['data']
             for item in data:
-                yield scrapy.Request(url=f'{url}/{item[id]}', 
+                yield scrapy.Request(url=f'{article_url}/{item[id]}', 
                     callback=self.parse(item['publish_time'], item['title']))
 
 
@@ -28,7 +30,7 @@ class VcbeatSpider(scrapy.Spider):
             content = response.xpath('//div[@class="row"]/div[@id="article-detail"]/div[@id="article-content"]/p/span/text()').extract()
             cntx = ''.join(content)
             filename = f'{date}:{title}.txt'
-            with open(f'data/{filename}', 'w') as f:
+            with open(f'vcbeat/{filename}', 'w') as f:
                 f.write(cntx)
             self.log('Saved file %s' % filename)
         return lam
