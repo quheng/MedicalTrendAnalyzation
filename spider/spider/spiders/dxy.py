7424757a67fc20bac03f05b6495678fa4440092a
quheng <494862190@qq.com>
2017-05-01 22:15:58 +0800

    fuck dxy

--- spider/spider/spiders/dxy.py
+++ spider/spider/spiders/dxy.py
@@ -8,19 +8,16 @@ class DxySpider(scrapy.Spider):
        
     def parse(self, response): 
         for div in response.css('div.main-list'):
-            href = div.css('h3 a::attr(href)').extract_first()
+            href = div.css('h3 a::attr(href)').extract_first().split('?')[0]
+            date = div.css('p.it-author::text').extract()[2].strip()
+            print('!!!!!!!!!!!!!!!!!!!!!!!!!!')
             print(href)
-            date = div.css('p.it-author::text').extract()[2]
-            yield scrapy.Request(response.urljoin(href),
-                                    callback=self.herf_with_date(date))
-
+            yield scrapy.Request(href, callback=self.herf_with_date(date))
 
     def herf_with_date(self, date):
         def href(response):
-            title = response.css('title::text').extract_first()
-            cntx = ''.join(response.css('div.content p::text').extract())
-            filename = '%s.txt' % sat
-            with open(f'dxy/{filename}', 'w') as f:
-                f.write(cntx)
-            self.log('Saved file %s' % filename)
+            print("!!!!!!")
+            print(response.url)
+            print(response.text)
+            print(dir(response))
         return href
