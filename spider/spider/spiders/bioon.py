a5d57ce72e51365c3b811607abd33cb07ff61878
quheng <494862190@qq.com>
2017-05-01 15:59:27 +0800

    update spider

--- spider/spider/spiders/bioon.py
+++ spider/spider/spiders/bioon.py
@@ -11,12 +11,6 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # response.css('li.select a::text').extract()
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
         if response.css('li.select a::text').extract_first() in self.category:
             date = response.css('div.title5 p::text').extract_first().split(' ')[1]
             title = response.css('title::text').extract_first()
